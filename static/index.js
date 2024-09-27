b8581ec4cb1050ab586c61b172f73f2a0821050e
Le Vivilet <levivlet@pm.me>
2024-09-27 18:10:51 +0200

    try to set src

--- static/index.js
+++ static/index.js
@@ -1,2 +1,8 @@
 const response = await fetch("./big_buck_bunny.mp4");
 const blob = await response.blob();
+
+const video = document.createElement("video");
+
+video.srcObject = blob;
+
+document.body.append(video);
