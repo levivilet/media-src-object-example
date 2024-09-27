494f32087588aa3d8bd0a159b2f5c01d03a89ba6
Le Vivilet <levivlet@pm.me>
2024-09-27 18:16:14 +0200

    debug mediasource

--- static/index.js
+++ static/index.js
@@ -3,6 +3,13 @@ const blob = await response.blob();
 
 const video = document.createElement("video");
 
-video.srcObject = blob;
+const mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
+
+video.src = blob;
+
+const m = new MediaSource();
+
+// const sourceBuffer = m.addSourceBuffer(mimeCodec);
+// sourceBuffer.appendBuffer(data);
 
 document.body.append(video);
